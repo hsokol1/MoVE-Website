043e1dc8f60f30e42d60b8e020352eb44dcae74d
hsokol1 <hsokoliouk@gmail.com>
2026-03-04 05:37:08 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,9 +2,10 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from app.queries import fetch_move_equation_scores, fetch_state_counties_census_data
+from app.queries import extraction, fetch_move_equation_scores, fetch_state_counties_census_data, get_state_ids_from_abbrevs, get_variable_lists
 from fastapi import Query, HTTPException
 import pandas as pd
+import numpy as np
 
 app = FastAPI()
 
@@ -63,20 +64,62 @@ def state_census_data(state_id: str):
         raise HTTPException(status_code=404, detail="State not found")
     return census_data
 
-@app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+@app.get("/extract")
+def extract(request: Request):
+    return templates.TemplateResponse(
+        "extract.html",
+        {"request": request}
+    )
+
+@app.get("/variables")
+def variables(request: Request):
+    return templates.TemplateResponse(
+        "variables.html",
+        {"request": request}
+    )
+
+@app.get("/all-variables")
+def get_all_variables():
+   variables = get_variable_lists()
+   return variables
 
 
-@app.get("/about", response_class=HTMLResponse)
-async def about(request: Request):
-    return templates.TemplateResponse("aboutus.html", {"request": request})
 
+@app.post("/data-extract")
+def data_extract(variables: dict):
+    level = variables["geography"]["level"]
+    if level == "county":
+        counties = variables["geography"]["counties"]
+        df = extraction("county", counties, variables)
+    else:
+        states = variables["geography"]["states"]
+        state_ids = get_state_ids_from_abbrevs(states)
+        df = extraction("state", state_ids, variables)
+    
+    data = (
+        df.replace({pd.NA: None})
+        .replace({np.nan: None, np.inf: None, -np.inf: None})
+        .to_dict(orient="records")
+    )
+    print({
+        "status": "ok",
+        "rows": len(df),
+        "data": data
+    })
+    
+    return {
+        "status": "ok",
+        "rows": len(df),
+        "data": data
+    }
 
 @app.get("/methods", response_class=HTMLResponse)
 async def methods(request: Request):
     return templates.TemplateResponse("methods.html", {"request": request})
 
+@app.get("/about", response_class=HTMLResponse)
+async def about_us(request: Request):
+    return templates.TemplateResponse("aboutus.html", {"request": request})
 
 @app.get("/publications", response_class=HTMLResponse)
 async def publications(request: Request):
